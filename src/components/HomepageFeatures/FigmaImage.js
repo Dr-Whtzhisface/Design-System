f8a885e50c23dc5938c1af6e52fc229132407d90
Dr-Whtzhisface <patrick.mead@hypergiant.com>
2023-08-15 12:59:45 -0400

    figma code

--- src/components/HomepageFeatures/FigmaImage.js
+++ src/components/HomepageFeatures/FigmaImage.js
@@ -1,14 +1,21 @@
 import React, { useEffect, useState } from 'react';
 
-const FigmaImage = ({ figmaNodeId, figmaScale, figmaFormat, figmaApiToken }) => {
+const FigmaImage = () => {
   const [imageUrl, setImageUrl] = useState('');
 
   useEffect(() => {
+    const figmaNodeId = '1:1045';
+    const figmaScale = '1';
+    const figmaFormat = 'png';
+    const figmaApiToken = 'figd_mcFPFGozK2HCuON5VTyrpvNyGien_J8BJsMUxak2';
+    const fileToken = '4vMAPIHT9iqOuhqF4N97kp';
+
     const fetchFigmaImage = async () => {
       try {
         const response = await fetch(`https://api.figma.com/v1/images/${figmaNodeId}?scale=${figmaScale}&format=${figmaFormat}`, {
           headers: {
-            'X-FIGMA-TOKEN': figmaApiToken
+            'X-FIGMA-TOKEN': figmaApiToken,
+            'X-FIGMA-FILE-TOKEN': fileToken
           }
         });
 
@@ -20,9 +27,10 @@ const FigmaImage = ({ figmaNodeId, figmaScale, figmaFormat, figmaApiToken }) => 
     };
 
     fetchFigmaImage();
-  }, [figmaNodeId, figmaScale, figmaFormat, figmaApiToken]);
+  }, []);
 
   return <img src={imageUrl} alt="Figma Image" />;
 };
 
 export default FigmaImage;
+

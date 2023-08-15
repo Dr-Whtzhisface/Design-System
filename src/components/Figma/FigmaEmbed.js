abbab1ad073f3d6f5f411711f46e2d3ed2b990bb
Dr-Whtzhisface <patrick.mead@hypergiant.com>
2023-08-15 16:26:21 -0400

    ufg

--- src/components/Figma/FigmaEmbed.js
+++ src/components/Figma/FigmaEmbed.js
@@ -1,10 +1,13 @@
-// src/components/FigmaEmbed.js
-
 import React from 'react';
 import { generateFigmaImageUrl } from '../utils/figmaUtils'; // Adjust the path as needed
 
 const FigmaEmbed = ({ fileToken, apiToken, nodeId, scale, format }) => {
-  const figmaImageUrl = "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/638ec794-c976-44e7-9f6c-1a61ca34f51d";//generateFigmaImageUrl(fileToken, apiToken, nodeId, scale, format);
+  console.log('fileToken:', fileToken);
+  console.log('apiToken:', apiToken);
+  console.log('nodeId:', nodeId);
+
+  const figmaImageUrl = generateFigmaImageUrl(fileToken, apiToken, nodeId, scale, format);
+  console.log('figmaImageUrl:', figmaImageUrl);
 
   return (
     <div>
@@ -12,7 +15,7 @@ const FigmaEmbed = ({ fileToken, apiToken, nodeId, scale, format }) => {
         <iframe
           title="Figma Embed"
           src={figmaImageUrl}
-          width="600"
+          width="100%"
           height="400"
           frameBorder="0"
         />

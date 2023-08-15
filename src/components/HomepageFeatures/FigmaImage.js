f005816317b9672caac79613d7245438a0120435
Dr-Whtzhisface <patrick.mead@hypergiant.com>
2023-08-15 13:18:27 -0400

    image change

--- src/components/HomepageFeatures/FigmaImage.js
+++ src/components/HomepageFeatures/FigmaImage.js
@@ -1,20 +1,18 @@
 import React, { useEffect, useState } from 'react';
 
-const FigmaImage = () => {
+const FigmaImage = ({ fileToken, apiToken }) => {
   const [imageUrl, setImageUrl] = useState('');
 
   useEffect(() => {
     const figmaNodeId = '1:1045';
     const figmaScale = '1';
     const figmaFormat = 'png';
-    const figmaApiToken = 'figd_mcFPFGozK2HCuON5VTyrpvNyGien_J8BJsMUxak2';
-    const fileToken = '4vMAPIHT9iqOuhqF4N97kp';
 
     const fetchFigmaImage = async () => {
       try {
         const response = await fetch(`https://api.figma.com/v1/images/${figmaNodeId}?scale=${figmaScale}&format=${figmaFormat}`, {
           headers: {
-            'X-FIGMA-TOKEN': figmaApiToken,
+            'X-FIGMA-TOKEN': apiToken,
             'X-FIGMA-FILE-TOKEN': fileToken
           }
         });
@@ -27,10 +25,16 @@ const FigmaImage = () => {
     };
 
     fetchFigmaImage();
-  }, []);
+  }, [fileToken, apiToken]);
 
-  return <img src={imageUrl} alt="Figma Image" />;
+  return (
+    <div>
+      <p>Generated Figma Image URL:</p>
+      <p>{imageUrl}</p>
+    </div>
+  );
 };
 
 export default FigmaImage;
+
 
